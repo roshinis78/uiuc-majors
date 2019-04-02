739b3ec8573cac87b6fb6b94f16076757678b019
xz33 <xz33@illinois.edu>
2019-04-02 14:06:13 -0500

    Create basic visualization and iteractive elements

--- visualization.js
+++ visualization.js
@@ -14,7 +14,7 @@ $(function() {
 var visualize = function(data) {
   // Boilerplate:
   var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-     width = 960 - margin.left - margin.right,
+     width = 1000 - margin.left - margin.right,
      height = 500 - margin.top - margin.bottom;
       
   var svg = d3.select("#chart")
@@ -27,5 +27,194 @@ var visualize = function(data) {
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Visualization Code:
+
+  var config = {
+    numColleges: 7,
+    spacing: 50,
+    labelPosition: [0, 75, 280, 390, 510, 650, 850],
+    colPosition: [20, 160, 310, 425, 550, 720, 865]
+  }
+
+  // get an array of all the colleges
+  var allColleges = [...new Set(data.map(item => item.College))];
+  
+
+  console.log(allColleges);
+
+  svg.selectAll('text')
+     .data(allColleges)
+     .enter()
+     .append('text')
+     .text(function(d, i) {
+        return allColleges[i];
+     })
+     .attr('x', function(d, i) {
+      return config.labelPosition[i];
+     })
+     .attr('y', height + 40)
+
+
+  var vertScale = d3.scaleLinear()
+                    .domain([1200, 0]) // get the maximum number
+                    .range([0, height]);
+
+  var positionMap = new Map();
+  allColleges.forEach(function(element, index) {
+    positionMap.set(element, config.colPosition[index]);
+  });
+
+  console.log(positionMap);
+
+
+   var startX = [];
+   var startY = [];
+   var endX = [];
+   var endY = [];
+
+  svg.selectAll('left')
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("class", "left")
+     .attr("id", function(d, i) {
+     		return "start-" + i;
+     })
+     .attr("r", 3)
+     .attr("cx", function(d, i) {
+        var x = positionMap.get(d.College) - 30;
+        startX.push(x);
+        return x;
+     })
+     .attr("cy", function(d, i) {
+        var y = vertScale(d["2004"]);
+        startY.push(y);
+        return y;
+     })
+     .attr("opacity", 0.5);
+
+  svg.selectAll('right')
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("class", "right")
+     .attr("id", function(d, i) {
+     		return "end-" + i;
+     })
+     .attr("r", 3)
+     .attr("cx", function(d, i) {
+        var x = positionMap.get(d.College) + 30;
+        endX.push(x);
+        return x;
+     })
+     .attr("cy", function(d, i) {
+        var y = vertScale(d["2018"]);
+        endY.push(y);
+        return y;
+     })
+     .attr("opacity", 0.5);
+
+  console.log(startX);
+  console.log(startY);
+  console.log(endX);
+  console.log(endY);
+
+  svg.selectAll("number-left")
+  	  .data(data)
+  	  .enter()
+  	  .append("text")
+  	  .text(function(d) {
+  	  		return d["2004"];
+  	  })
+  	  .attr("x", function(d, i) {
+  	  		return startX[i] - 35;
+  	  })
+  	  .attr("y", function(d, i) {
+  	  		return startY[i] + 5;
+  	  })
+  	  .attr("class", "number-left")
+  	  .attr("id", function(d, i) {
+  	  		return "number-left-" + i;
+  	  })
+  	  .attr("opacity", 0);
+
+  	svg.selectAll("number-right")
+  	  .data(data)
+  	  .enter()
+  	  .append("text")
+  	  .text(function(d) {
+  	  		return d["2018"];
+  	  })
+  	  .attr("x", function(d, i) {
+  	  		return endX[i] + 10;
+  	  })
+  	  .attr("y", function(d, i) {
+  	  		return endY[i] + 5;
+  	  })
+  	  .attr("class", "number-right")
+  	  .attr("id", function(d, i) {
+  	  		return "number-right-" + i;
+  	  })
+  	  .attr("opacity", 0);
+
+  svg.selectAll("slope-line")
+     .data(data)
+     .enter()
+     .append("line")
+     .attr("class", "slope-line")
+     .attr("id", function(d, i) {
+     	  return "line-" + i;
+     })
+     .attr("x1", function(d, i) {
+        return startX[i];
+     })
+     .attr("y1", function(d, i) {
+        return startY[i];
+     })
+     .attr("x2", function(d, i) {
+        return endX[i];
+     })
+     .attr("y2", function(d, i) {
+        return endY[i];
+     })
+     .attr("stroke-width", 1)
+     .attr("stroke", "black")
+     .attr("opacity", 0.5);
+
+
+     svg.selectAll("slope-line-area")
+     .data(data)
+     .enter()
+     .append("line")
+     .attr("class", "slope-line-area")
+     .attr("x1", function(d, i) {
+        return startX[i];
+     })
+     .attr("y1", function(d, i) {
+        return startY[i];
+     })
+     .attr("x2", function(d, i) {
+        return endX[i];
+     })
+     .attr("y2", function(d, i) {
+        return endY[i];
+     })
+     .attr("stroke-width", 20)
+     .attr("stroke", "white")
+     .attr("opacity", 0)
+     .on("mouseover", function(d, i){
+     		document.getElementById("line-" + i).style.opacity = 1;
+     		document.getElementById("start-" + i).style.opacity = 1;
+     		document.getElementById("end-" + i).style.opacity = 1;
+     		document.getElementById("number-left-" + i).style.opacity = 1;
+     		document.getElementById("number-right-" + i).style.opacity = 1;
+     }).on("mouseout", function(d, i){
+     		document.getElementById("line-" + i).style.opacity = 0.5;
+     		document.getElementById("start-" + i).style.opacity = 0.5;
+     		document.getElementById("end-" + i).style.opacity = 0.5;
+     		document.getElementById("number-left-" + i).style.opacity = 0;
+     		document.getElementById("number-right-" + i).style.opacity = 0;
+     });
+
+
 
 };
